d422ef7b39cb36e5cbd448d9bf74f5f5b3197d5a
asheq1 <bsmrstu18fmb025@gmail.com>
2024-09-10 19:49:36 +0600

    react awesome components

--- src/components/Axios/Axios.jsx
+++ src/components/Axios/Axios.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
+import { Audio } from 'react-loader-spinner'
 
 const Axios = () => {
     const [phones, setPhone] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         axios.get(`https://openapi.programming-hero.com/api/phones?search=iphone`)
@@ -17,7 +19,7 @@ const Axios = () => {
                 return obj;
             });
             setPhone(phoneWithData);
-            console.log(phoneWithData);
+            setLoading(false)
         })
         .catch(error => {
             console.error("Error fetching data:", error);
@@ -26,6 +28,19 @@ const Axios = () => {
 
     return (
         <div>
+            {
+                loading && <div className='mx-auto'>
+                    <Audio
+                    height="80"
+                    width="80"
+                    radius="9"
+                    color="green"
+                    ariaLabel="loading"
+                    wrapperStyle
+                    wrapperClass
+                    />
+                </div>
+            }
             <h2>Phone Count: {phones.length}</h2>
             <BarChart width={1200} height={400} data={phones}>
                 <XAxis dataKey="name" />
